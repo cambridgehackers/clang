479b1d2c2dd59f7368a92df1c5fcfe51e3fb3940
John Ankcorn <jca@alum.mit.edu>
2017-12-30 09:16:18 -0800

    cleanup rules processing

--- lib/CodeGen/CGRules.cpp
+++ lib/CodeGen/CGRules.cpp
@@ -32,13 +32,10 @@ using namespace CodeGen;
 llvm::Value *CodeGenFunction::EmitRuleLiteral(const RuleExpr *blockExpr) {
   // Allocate the block info and place it at the head of the list.
   const BlockDecl *blockDecl = blockExpr->getBlockDecl(); 
-  CGBlockInfo &blockInfo = *new CGBlockInfo(blockDecl, "");
-  blockInfo.NextBlockInfo = FirstBlockInfo;
-  FirstBlockInfo = &blockInfo; 
+  QualType longType = CGM.getContext().LongTy; // all captured data now stored as i64
   SourceLocation loc;
 
   QualType thisType = cast<CXXMethodDecl>(CurFuncDecl)->getThisType(CGM.getContext());
-  const FunctionProtoType *FnType = blockExpr->getFunctionType();
   FunctionArgList Args; 
   IdentifierInfo *IThis = &CGM.getContext().Idents.get("this"); 
   Args.push_back(ParmVarDecl::Create(getContext(), const_cast<BlockDecl *>(blockDecl), loc,
@@ -49,7 +46,6 @@ llvm::Value *CodeGenFunction::EmitRuleLiteral(const RuleExpr *blockExpr) {
   SmallVector<llvm::Type*, 8> elementTypes;
   elementTypes.push_back(CGM.VoidPtrTy); // void *invoke;
   elementTypes.push_back(CGM.Int64Ty);   // i64   STy;
-  QualType VTl = CGM.getContext().LongTy; // all captured data now stored as i64
 
   // Next, all the block captures.
   int pindex = elementTypes.size();
@@ -60,32 +56,22 @@ llvm::Value *CodeGenFunction::EmitRuleLiteral(const RuleExpr *blockExpr) {
      || CI.hasCopyExpr() || CI.isNested() || VT->isReferenceType()) {
 printf("[%s:%d]ZZZZZ\n", __FUNCTION__, __LINE__); exit(-1);
     }
-    elementTypes.push_back(CGM.getTypes().ConvertTypeForMem(VTl));
+    elementTypes.push_back(CGM.getTypes().ConvertTypeForMem(longType));
     IdentifierInfo *II = &CGM.getContext().Idents.get(CI.getVariable()->getName()); 
     Args.push_back(ParmVarDecl::Create(getContext(), const_cast<BlockDecl *>(blockDecl), loc,
         loc, II, CI.getVariable()->getType(), /*TInfo=*/nullptr, SC_None, nullptr));
   }
-  const CGFunctionInfo &FnInfo = CGM.getTypes().arrangeBlockFunctionDeclaration(FnType, Args);
-  llvm::Function *Fn = llvm::Function::Create(CGM.getTypes().GetFunctionType(FnInfo),
-      llvm::GlobalValue::ExternalLinkage, //why is this not allowed?? llvm::GlobalValue::InternalLinkage
-      "ruleTemplate", &CGM.getModule());
-
-  auto AI = Fn->arg_begin();
-  AI++; // skip 'this' parameter
-  for (const auto &CI : blockDecl->captures()) {
-      blockInfo.paramMap[CI.getVariable()] = AI;
-      AI++;
-  }
-
-  llvm::Constant *blockFn = llvm::ConstantExpr::getBitCast(
-      CodeGenFunction(CGM, true).GenerateRuleFunction(CurGD, blockInfo, FnType->getReturnType(),
-         Fn, FnInfo, Args),
-      VoidPtrTy);
-
   llvm::StructType *StructureType = llvm::StructType::get(CGM.getLLVMContext(), elementTypes, true);
 printf("[%s:%d] STRUCTURETYPE \n", __FUNCTION__, __LINE__);
 StructureType->dump();
   const llvm::StructLayout *layout = CGM.getContext().getTargetInfo().getDataLayout().getStructLayout(StructureType);
+
+  const CGFunctionInfo &FnInfo = CGM.getTypes().arrangeBlockFunctionDeclaration(
+        blockExpr->getFunctionType(), Args);
+  llvm::Function *Fn = llvm::Function::Create(CGM.getTypes().GetFunctionType(FnInfo),
+      llvm::GlobalValue::ExternalLinkage, //why is this not allowed?? llvm::GlobalValue::InternalLinkage
+      "ruleTemplate", &CGM.getModule());
+
   // Make the allocation for the block.
   Address blockAddr = CreateTempAlloca(StructureType, CGM.getPointerAlign(), "block"); 
 
@@ -96,8 +82,9 @@ StructureType->dump();
   auto storeField = [&](llvm::Value *value, unsigned index, CharUnits offset, const Twine &name) {
       Builder.CreateStore(value, projectField(index, offset, name));
     };
-  storeField(blockFn, 0, CharUnits(), "block.invoke");             // Function *invoke;
-  storeField(llvm::Constant::getIntegerValue(CGM.Int64Ty,          // Int64Ty STy;
+  storeField(llvm::ConstantExpr::getBitCast(Fn, VoidPtrTy),
+      0, CharUnits(), "block.invoke");                     // Function *invoke;
+  storeField(llvm::Constant::getIntegerValue(CGM.Int64Ty,  // Int64Ty STy;
     llvm::APInt(64, (uint64_t) StructureType)), 1, CharUnits(), "block.STy");
 
   // Finally, capture all the values into the block.
@@ -106,19 +93,33 @@ StructureType->dump();
     QualType VT = variable->getType();
     // Fake up a new variable so that EmitScalarInit doesn't think
     // we're referring to the variable in its own initializer.
-    ImplicitParamDecl BlockFieldPseudoVar(getContext(), VTl, ImplicitParamDecl::Other); 
-    DeclRefExpr declRef(const_cast<VarDecl *>(variable), /*RefersToEnclosingVariableOrCapture*/ false,
-                        VT, VK_LValue, SourceLocation()); 
-    ImplicitCastExpr l2r(ImplicitCastExpr::OnStack, VT, CK_LValueToRValue, &declRef, VK_RValue);
-    Expr *rval = &l2r;
-    if (VT != VTl)
-        rval = ImplicitCastExpr::Create(CGM.getContext(), VTl, CK_IntegralCast,  &l2r, nullptr, VK_RValue);
-    LValueBaseInfo BaseInfo(AlignmentSource::Decl, false);
+    DeclRefExpr declRef(const_cast<VarDecl *>(variable),
+        /*RefersToEnclosingVariableOrCapture*/ false, VT, VK_LValue, SourceLocation()); 
+    Expr *rval = ImplicitCastExpr::Create(CGM.getContext(), VT,
+          CK_LValueToRValue, &declRef, nullptr, VK_RValue);
+    if (VT != longType)
+        rval = ImplicitCastExpr::Create(CGM.getContext(), longType,
+              CK_IntegralCast, rval, nullptr, VK_RValue);
+    ImplicitParamDecl BlockFieldPseudoVar(getContext(), longType, ImplicitParamDecl::Other); 
     EmitExprAsInit(rval, &BlockFieldPseudoVar, MakeAddrLValue(
         projectField(pindex, CharUnits::fromQuantity(layout->getElementOffset(pindex)), "block.captured"),
-        VTl, BaseInfo), /*captured by init*/ false);
+        longType, LValueBaseInfo(AlignmentSource::Decl, false)), /*captured by init*/ false);
     pindex++;
   } 
+
+  // Now generate function itself
+  CGBlockInfo &blockInfo = *new CGBlockInfo(blockDecl, "");
+  blockInfo.NextBlockInfo = FirstBlockInfo;
+  FirstBlockInfo = &blockInfo; 
+  auto AI = Fn->arg_begin();
+  AI++; // skip 'this' parameter
+  for (const auto &CI : blockDecl->captures()) {
+      blockInfo.paramMap[CI.getVariable()] = AI;
+      AI++;
+  }
+  CodeGenFunction(CGM, true).GenerateRuleFunction(CurGD, blockInfo, FnInfo.getReturnType(),
+         Fn, FnInfo, Args);
+
   // Cast to the converted block-pointer type, which happens (somewhat
   // unfortunately) to be a pointer to function type.
   return Builder.CreatePointerCast(blockAddr.getPointer(), ConvertType(blockExpr->getType()));
