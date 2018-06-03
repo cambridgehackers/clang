406ab69fe8f5779e6c39cde9a4b4385a8d39c7ab
John Ankcorn <jca@alum.mit.edu>
2018-06-02 19:26:32 -0700

    added template processing in SemaType

--- lib/Sema/SemaType.cpp
+++ lib/Sema/SemaType.cpp
@@ -6970,6 +6970,35 @@ static void processTypeAttrs(TypeProcessingState &state, QualType &type,
       break;
 
     case AttributeList::AT_AtomiccWidth:
+#if 1
+{
+  Expr *E = attr.getArgAsExpr(0);
+  if (!type->isIntegerType()) {
+    printf("[%s:%d] atomicc_width not integer type\n", __FUNCTION__, __LINE__);
+    //Diag(AttrLoc, diag::warn_attribute_pointer_or_reference_only)
+      //<< &TmpAttr /*TmpAttr.getName()*/ << T << D->getSourceRange();
+    //return;
+  }
+  if (!E->isValueDependent()) {
+    unsigned DestWidth = 9;
+    llvm::APSInt itemWidth(32);
+    if (E->isIntegerConstantExpr(itemWidth, state.getSema().Context)) {
+      DestWidth = itemWidth.getZExtValue();
+    }
+    else
+      printf("[%s:%d] NOTINTEGERLITERAL\n", __FUNCTION__, __LINE__);
+    BuiltinType *Ty = new (state.getSema().Context, TypeAlignment) BuiltinType(BuiltinType::Int);
+    Ty->atomiccWidth = DestWidth;
+    type = QualType(Ty, 0);
+printf("[%s:%d] SSSSSSSSWWWWWWWWWWWWWWWWWW width %d\n", __FUNCTION__, __LINE__, DestWidth);
+type.dump();
+  }
+else {
+printf("[%s:%d] VVVVVVVVVVVAAAAAAAAALLLLLLLLUEDEP\n", __FUNCTION__, __LINE__);
+E->dump();
+}
+}
+#endif
       attr.setUsedAsTypeAttr();
       break;
     }
